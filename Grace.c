954a3da9844201c4c5e59aa19f4501db0da56465
Ayoub Ben Brahim <aybouras@student.1337.ma>
2024-01-17 17:58:46 +0100

    Grace

--- Grace.c
+++ Grace.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
-#define PROGRAM "#include <stdio.h>%1$c%1$c#define PROGRAM %2$c%3$s%2$c%1$c%1$c#define WRITE_PROGRAM() fprintf(fp, PROGRAM, 10, 34, PROGRAM)%1$c#define EXECUTE_PROGRAM() int main() { FILE *fp = fopen(%2$cGrace_kid.c%2$c, %2$cw%2$c); if (fp != NULL) { WRITE_PROGRAM(); fclose(fp); } return 0; }%1$c%1$cEXECUTE_PROGRAM()%1$c"
+ /*
+  Some Comment
+ */
+
+#define PROGRAM "#include <stdio.h>%1$c%1$c /*%1$c  Some Comment%1$c */%1$c%1$c#define PROGRAM %2$c%3$s%2$c%1$c%1$c#define WRITE_PROGRAM() fprintf(fp, PROGRAM, 10, 34, PROGRAM)%1$c#define FT(X) int main() { FILE *fp = fopen(X, %2$cw%2$c); if (fp != NULL) { WRITE_PROGRAM(); fclose(fp); } return 0; }%1$c%1$cFT(%2$cGrace_kid.c%2$c)%1$c"
 
 #define WRITE_PROGRAM() fprintf(fp, PROGRAM, 10, 34, PROGRAM)
-#define EXECUTE_PROGRAM() int main() { FILE *fp = fopen("Grace_kid.c", "w"); if (fp != NULL) { WRITE_PROGRAM(); fclose(fp); } return 0; }
+#define FT(X) int main() { FILE *fp = fopen(X, "w"); if (fp != NULL) { WRITE_PROGRAM(); fclose(fp); } return 0; }
 
-EXECUTE_PROGRAM()
+FT("Grace_kid.c")
